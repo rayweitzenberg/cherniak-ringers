e31b8fe8c5e96d7ef6342a846799e25ef17838e2
Ray Weitzenberg <ray@mechaneyes.com>
2022-11-05 12:09:18 -0700

    dude. works 🎯
    🏄‍♂️ carousel advancing, but updatting its slide contents beforehand

--- pages/ringers.js
+++ pages/ringers.js
@@ -4,7 +4,7 @@ import Carousel from "react-bootstrap/Carousel";
 import generateNum from "../components/generateNum";
 import RingerImage from "../components/ringerImage";
 
-// Ringers on art blocks:
+// Ringers on art blocks courtesy of @dmitricherniak
 // https://www.artblocks.io/collections/curated/projects/0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270/13
 
 // https://nextjs.org/docs/api-reference/next/image#advanced-props
@@ -16,36 +16,28 @@ export default function Ringers() {
   const [activeRinger, setActiveRinger] = useState(true);
 
   useEffect(() => {
-    generateNum()
-      .then((res) => {
-        console.log("res", res);
-        setRingerNum(res);
-      })
+    generateNum().then((res) => {
+      console.log("res", res);
+      !activeRinger ? setRingerNum(res) : setNextRingerNum(res);
+    });
   }, [activeRinger]);
 
-  let doneSlid = () => {
-    setNextRingerNum(ringerNum);
-
+  let dunSlid = () => {
     setTimeout(() => {
       setActiveRinger(!activeRinger);
-      console.log("activeRinger", activeRinger);
+      //   console.log("activeRinger", activeRinger);
     }, 4800);
-
-    // let nextSlide = document.querySelectorAll(".carousel-item:not(.active)");
   };
 
   return (
     <>
       <div className="ringer-page">
-        <Carousel onSlid={doneSlid} controls={true}>
-          {/* <Carousel.Item>
-            {activeRinger ? (
-              <RingerImage ringerNum={ringerNum} />
-            ) : (
-              <RingerImage ringerNum={nextRingerNum} />
-            )}
-          </Carousel.Item> */}
-
+        <Carousel
+          interval={5000}
+          onSlid={dunSlid}
+          controls={false}
+          indicators={false}
+        >
           <Carousel.Item>
             <RingerImage ringerNum={ringerNum} />
           </Carousel.Item>
